2cbacc85210a4c574ff0f8713c4126dec569de8b
UnlockEmulation <lucashardy1232@outlook.com>
2022-06-16 00:30:57 -0400

    Made file, added inital imports and comments

---
+++ cogs/Music controls.py
@@ -0,0 +1,14 @@
+'''
+what this page does:
+Holds all commands related to music controls
+
+TODO: Add searching for music though youtube
+TODO: Add playing songs in a voice channel
+TODO: 
+
+'''
+
+
+from discord.ext import commands
+import discord
+
